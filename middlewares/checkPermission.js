64c5123c382189f790b61435a96c8f24a8fdda42
aslamjon ibragimov <aslamjon.ibragimov@gmail.com>
2021-10-29 15:25:34 +0000

    sales

--- middlewares/checkPermission.js
+++ middlewares/checkPermission.js
@@ -1,10 +1,16 @@
 
-
+/* rules of users
+    admin
+    builder
+    sales
+    control
+*/
 function checkPermission(req, res, next) {
     const {rule} = req.user;
     // console.log(rule)
     if (rule == 'admin') {
     } else if (rule == 'builder') {
+    } else if (rule == 'sales') {
     } else {
         res.send({ message: "You can not access here" })
     }
